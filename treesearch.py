a08ba7c3ee7fc4e95046dc72a08a9d14e9dd0179
mconati <mcconati@gmail.com>
2021-01-04 18:04:21 -0600

    Third Commit

--- treesearch.py
+++ treesearch.py
@@ -28,3 +28,23 @@ board = chess.Board()
 print(board)
 prior_p, state_val = fakeNN(board)
 print(prior_p, state_val)
+
+
+def MCTS(Node n):
+    children = n.children
+    if(children ==[]):
+        prior_p, state_val = fakeNN(n.board)
+        children_moves = board.legal_moves
+        for x in range(0, len(children_moves)):
+            move = n.board.push_san(children_moves[x])
+            node = Node(move, [0, 0, 0, prior_p[x]])
+            n.children.append(node)
+            node.parent.append(n)
+            n.visit_ct += 1
+            n.total_val += state_val
+            n.mean_val = n.total_val/n.visit_ct
+            return state_val
+    #Keep recursing. First pick child to maximize Q+U. Second rootnode +=MCTS(child node), visit_ct +1,update total_val and mean_val.
+    else():
+        
+
